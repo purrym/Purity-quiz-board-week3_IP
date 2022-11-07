cdf1244523f3fb9bcdadae4ed39a2e3ad8614af8
Purity Wanjira Munene <PMunene@safaricom.co.ke>
2022-11-07 09:38:18 +0300

    links to boostrap added
    clear
    git status
    
    ok

---
+++ main .js
@@ -0,0 +1,69 @@
+const myAnswers = document.querySelectorAll(".ouranswers");
+
+const finishButton = document.querySelector("#submit_ans")
+
+
+
+let total = 0;
+
+let score = 0;
+myAnswers.forEach(function (answer) {
+
+    total += parseInt(answer.value);
+
+})
+finishButton.addEventListener("click", function (e) {
+
+
+
+    myAnswers.forEach(function (answer) {
+
+        if (answer.checked) {
+
+            score += parseInt(answer.value);
+
+        }
+
+
+
+    })
+
+
+
+
+
+
+
+    let percentageScore = (score / total) * 100;
+
+    let comments = '';
+
+
+
+    if (percentageScore > 80) {
+
+        comments += "Excellent! You have scored " + percentageScore + "%";
+
+    }
+
+    else if (percentageScore >= 50 && percentageScore <= 80) {
+
+        comments += "Good! You have scored " + percentageScore + "%";
+
+    }
+
+
+
+    else {
+
+        comments += "Poor Donely! You have scored " + percentageScore + "%";
+
+    }
+
+
+
+    alert(comments)
+
+
+
+})
